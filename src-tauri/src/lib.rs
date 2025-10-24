83feb86c7d22f378caa432cdce34ca2ca1f6b477
EncVar <gbc034@outlook.com>
2025-10-24 13:50:42 +0800

    imp: tailwindcss

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,16 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            if cfg!(debug_assertions) {
+                app.get_window("main").unwrap().set_always_on_top(true)?;
+            }
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ src/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    import logo from "./assets/jxfls.png"
+</script>
+
+<template>
+    <div class="draggable h-20 w-full bg-white absolute">
+        <img v-bind:src="logo" alt="" class="scale-15 absolute ml-1 mt-auto ms-auto">
+    </div>
+</template>
+
+<style scoped>
+    div.draggable {
+        --webkit-app-region: drag;
+    }
+</style>
