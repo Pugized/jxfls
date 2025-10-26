245430e67a43fe0df770d8adf9d656777dd0358d
EncVar <gbc034@outlook.com>
2025-10-26 23:15:24 +0800

    feat: traffic light

--- src/NavBar.vue
+++ src/NavBar.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
     import logo from "./assets/jxfls.png"
+    import { isTauri } from "@tauri-apps/api/core";
+    import TrafficLight from "./TrafficLight.vue";
+    let bar_class = "h-15 w-full bg-white pl-0 absolute border-b-1 border-slate-300 basis-2 flex flex-row " + (isTauri() ? "draggable" : "");
 </script>
 
 <template>
-    <div class="draggable h-20 w-full bg-white absolute">
-        <img v-bind:src="logo" alt="" class="scale-15 absolute ml-1 mt-auto ms-auto">
+    <div v-bind:class="bar_class">
+        <TrafficLight class="ml-1 "/>
+        <img v-bind:src="logo" alt="" class="h-10 relative mt-2 ml-3"/>
     </div>
 </template>
 
 <style scoped>
     div.draggable {
-        --webkit-app-region: drag;
+        -webkit-app-region: drag;
     }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./tailwind.css"
+import { isTauri } from "@tauri-apps/api/core";
 
 createApp(App).mount("#app");
+console.log(isTauri());
 
-export const client: boolean = Object(window).hasOwnProperty("__TAURI__")
+export function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+    );
+}
